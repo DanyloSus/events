54868c1420886d4ba3ba92fd4535ad79b104d5b6
DanyloSus <danil.sushko19@gmail.com>
2024-04-20 12:31:26 +0300

    Add values to models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'comment'];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'start_time', 'end_time'];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
